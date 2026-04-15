af5fbc520c5c9932b3f3b6485a1bb8e342335ad8
Kirill <rerle@bwg-io.site>
2026-04-15 15:12:01 +0300

    fix main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,24 @@
 package main
 
+import (
+	"context"
+	"log"
 
-func main(){
+	"github.com/martketplace-vkr/blockchain-watcher/config"
+	"github.com/martketplace-vkr/blockchain-watcher/internal/app"
 
+	cfgloader "github.com/martketplace-vkr/pkg/config"
+)
+
+func main() {
+	ctx := context.Background()
+	cfg := &config.Config{}
+
+	if err := cfgloader.LoadConfig(ctx, cfg); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if err := app.Run(ctx, cfg); err != nil {
+		log.Fatal(err.Error())
+	}
 }
